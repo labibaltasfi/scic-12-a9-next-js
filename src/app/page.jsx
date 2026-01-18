b5d97f3162fc54a9698e4f0e9d16bb0aeac2e151
labibaltasfi <labibaltasfi5@gmail.com>
2026-01-18 19:45:06 +0600

    third commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
      <p>hello next js</p>
+     <h1>New line add</h1>
     </div>
   );
 }
